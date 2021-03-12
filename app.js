844dae237f2c216bcddece67bdc9e8fcfa458019
David Wu <david204code@gmail.com>
2021-03-12 12:56:55 +0000

    Can play/pause the video, select the html element for javaScript and addEventListener

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
 // MDN
 // The DOMContentLoaded event fires when the initial HTML document has been completely loaded and parsed, without waiting for stylesheets, images, and subframes to finish loading.
 // The load event is fired when the whole page has loaded, including all dependent resources such as stylesheets and images.
+
+// select the button and video container
+
+const btn = document.querySelector(".switch-btn");
+const video = document.querySelector(".video-container");
+
+btn.addEventListener("click", function () {
+  if (!btn.classList.contains("slide")) {
+    btn.classList.add("slide");
+    // control the video method
+    video.pause();
+  } else {
+    btn.classList.remove("slide");
+    // control the video method
+    video.play();
+  }
+});
